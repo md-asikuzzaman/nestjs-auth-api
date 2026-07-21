fbdc1f41bac4ef02521df2b9eea2d3a1bc98b95a
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2026-07-21 16:58:06 +0600

    feat: add helmet middleware for enhanced security; update CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
+  app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
