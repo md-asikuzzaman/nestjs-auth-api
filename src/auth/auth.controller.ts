24e2fa12b76fd2b1b59b28e44dd104e5634e7ebe
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2026-07-21 16:17:21 +0600

    feat: add authentication routes for login, register, logout, and refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login() {
+    return { msg: 'This is the login route' };
+  }
+
+  @Post('register')
+  register() {
+    return { msg: 'This is the register route' };
+  }
+
+  @Post('logout')
+  logout() {
+    return { msg: 'This is the logout route' };
+  }
+
+  @Post('refresh')
+  refreshToken() {
+    return { msg: 'This is the refresh token route' };
+  }
 }
