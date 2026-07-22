ca360e4bdc8cd3dde79e9363ab4c0060dd8373e0
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2026-07-22 10:58:12 +0600

    feat: update Prisma configuration and service for improved integration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
@@ -9,5 +9,9 @@ export class PrismaService extends PrismaClient {
       connectionString: process.env.DATABASE_URL as string,
     });
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 }
