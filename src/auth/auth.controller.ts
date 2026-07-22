068ff685cb549e198aeefd7d0671a96c541efe33
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2026-07-22 12:39:27 +0600

    feat: add public access to register and login routes; implement admin-only route with role-based access control

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,16 +2,20 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { Message } from 'src/common/decorators/message.decorator';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from 'src/users/dto/auth.dto';
+import { Public } from './decorators/public.decorator';
+import { Roles } from './decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('register')
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
+  @Public()
   @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
@@ -26,5 +30,11 @@ export class AuthController {
   @Message('Token refreshed successfully')
   refreshToken() {
     return { msg: 'This is the refresh token route' };
+  }
+
+  @Roles('ADMIN')
+  @Post('admin-only')
+  adminRoute() {
+    return { message: 'Welcome to the admin panel' };
   }
 }
