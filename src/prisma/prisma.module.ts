f5c60fbfd5330ecd7ce90024b1e62fc56fb4535b
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2026-07-22 11:15:08 +0600

    feat: update database URL in .env.example and enhance User model in Prisma schema; remove unused Prisma controller

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { PrismaController } from './prisma.controller';
 
 @Global()
 @Module({
-  controllers: [PrismaController],
+  controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
 })
